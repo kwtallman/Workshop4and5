2848f83e6c8388ec0e3324a8c504f8b757ece2d2
Kyle Tallman <kyletallman1@gmail.com>
2017-04-04 22:22:34 -0400

    fb5

--- app/components/comment.js
+++ app/components/comment.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {unixTimeToString} from '../util.js';
+
 export default class Comment extends React.Component {
   render() {
     return (
@@ -8,9 +10,9 @@ export default class Comment extends React.Component {
           PIC
         </div>
         <div className="media-body">
-          <a href="#">{this.props.author}</a> {this.props.children}
+          <a href="#">{this.props.author.fullName}</a> {this.props.children}
           <br /><a href="#">Like</a> · <a href="#">Reply</a> ·
-            {this.props.postDate}
+            {unixTimeToString(this.props.postDate)}
         </div>
       </div>
     )
